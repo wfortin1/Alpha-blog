0d8f96c3b1e0f87f2d6c52840a519aa0fdde516b
wfortin1 <wfortin@uwo.ca>
2022-06-13 15:23:42 -0400

    Setup admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,9 +41,13 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles successfully deleted"
-    redirect_to articles_path
+    if @user == current_user
+      redirect_to articles_path
+    else
+      redirect_to users_path
+    end
   end
 
   private
@@ -57,8 +61,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only edit your own account"
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
       redirect_to @user
     end
   end
